ebd12f20b55a52b3f80c7f8461117c25b32c0c29
Kacper Smal <ksmal@future-processing.com>
2022-10-04 07:24:31 +0200

    added config

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import AddApplicationRoutes from './src/routes';
 import GlobalErrorHandler from './src/middleware/GlobalErrorHandler';
+import Config from "./config/Config";
 
 const app = express();
-const port = 3000;
+const port = Config.Server.Port;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -13,6 +14,6 @@ AddApplicationRoutes(app);
 app.use(GlobalErrorHandler)
 
 app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
+  console.log(`Server started at port: ${port}.`);
 });
 
---
+++ config/Config.ts
@@ -0,0 +1,17 @@
+const env = process.env.NODE_ENV;
+
+const Dev = {
+    Server: {
+        Port:3000
+    }
+}
+
+const Prod = {
+    Server: {
+        Port:3000
+    }
+}
+
+const Config = env === "production" ? Prod : Dev;
+
+export default Config;
